912c981f82a46ab50e219c062402e178e11f21ed
Sankalp Bansal <112388450+BoozeBear99@users.noreply.github.com>
2022-12-06 20:48:02 +0530

    Add files via upload

--- broken-keyboard.cpp
+++ broken-keyboard.cpp
@@ -13,36 +13,36 @@ You are given a word consisting of n lowercase Latin letters. You have to determ
 using namespace std;
 
 int main(){
-  int t,n,c,flag,j;							                                  //t stores number of terms
-										                                              //n stores length of string
-										                                              //flag is the check variable 1 for true 0 for false
+  int t,n,c,flag,j;							//t stores number of terms
+										//n stores length of string
+										//flag is the check variable 1 for true 0 for false
 
-  string s;									                                      //s stores the string input
-  char c1,c2;								                                      //c1 and c2 store adjacent characters	
+  string s;									//s stores the string input
+  char c1,c2;								//c1 and c2 store adjacent characters	
 
   cin >> t;
-  string result[t];							                                  //to store output
+  string result[t];							//to store output
 
   if(t>=1 && t<=100){							
     for(int i=1;i<=t;i++){
 	cin >> n;
 	cin >> s;
-	flag=1;c=0;								                          //to be reinitialized for each input
+	flag=1;c=0;								//to be reinitialized for each input
       if(n>=1 && n<=100 && s.length() == n){
-	  if((n+1)%3==0){result[i-1]="NO";continue;}		    //optimization---By analysis lengths cannot be equal to 2,5,8,11....or(in general (3r-1))
+	  if((n+1)%3==0){result[i-1]="NO";continue;}		//optimization---By analysis lengths cannot be equal to 2,5,8,11....or(in general (3r-1))
 	  for(j=0;j<n-1;j++){						
 	    if(c++%2!=0){
 		c1=s.at(j);c2=s.at(j+1);
 		if(c1!=c2){flag=0;break;}
-	      j++;								                          //incremented to make the loop directly jump to next pair as it is to be verified next
-	    }									                              //closing if to check c
-	  }									                                //closing for-loop traversing string
+	      j++;								//incremented to make the loop directly jump to next pair as it is to be verified next
+	    }									//closing if to check c
+	  }									//closing for-loop traversing string
 	  result[i-1]=(flag==0)?"NO":"YES";							
-      }									                              //closing if to check 'n'
-    }									                              	//closing for-loop controlling terms
+      }									//closing if to check 'n'
+    }										//closing for-loop controlling terms
 
     for(int i=0;i<t;i++)cout << result[i] << endl;		//printing result
 
-  }										                                //closing if to check 't'
+  }										//closing if to check 't'
   return 0;
-}										                                  //closing main()
+}										//closing main()
